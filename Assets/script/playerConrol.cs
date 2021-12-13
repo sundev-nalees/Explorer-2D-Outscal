af64ea95d93d89307e38839615e00e374f140508
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2021-12-13 21:48:15 +0530

    crouch and jumb

--- Assets/script/playerConrol.cs
+++ Assets/script/playerConrol.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class playerConrol : MonoBehaviour
 {
     public Animator animator;
+    bool crouch = false;
+    public float verticalSpeed = 2f;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        //run
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed",Mathf.Abs(speed));
-
+        
         Vector3 scale = transform.localScale;
         if (speed < 0) 
         {
@@ -28,5 +32,24 @@ public class playerConrol : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+        
+        //crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl)) 
+        {
+            crouch = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            crouch = false;
+        }
+        animator.SetBool("crouch", crouch);
+        //jumb
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.AddForce (new Vector2(0,verticalSpeed));
+            
+        }
+        animator.SetFloat("verticalSpeed", rb.velocity.y);
     }
 }
+        
