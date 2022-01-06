b0bf0ba00212a9df0e53b162d5469787aab9983a
sundev-nalees <72244488+sundev-nalees@users.noreply.github.com>
2022-01-06 20:23:15 +0530

    unlocking system

--- Assets/script/gameOverScreen.cs
+++ Assets/script/gameOverScreen.cs
@@ -7,18 +7,27 @@ using UnityEngine.SceneManagement;
 public class gameOverScreen : MonoBehaviour
 {
     public Button restart;
-
+    public string levelName;
+    public Button mainMenu;
     private void Awake()
     {
         restart.onClick.AddListener(restartGame);
+        mainMenu.onClick.AddListener(mainMenuScene);
+
     }
     public void playerDead()
     {
         gameObject.SetActive(true);
+        Debug.Log("dead");
     }
     private void restartGame()
     {
         int currentSceneNumber = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneNumber);
+    }
+    private void mainMenuScene()
+    {
+        SceneManager.LoadScene(levelName);
+
     }
 }
--- Assets/script/level script/levelFinishedCheck.cs
+++ Assets/script/level script/levelFinishedCheck.cs
@@ -5,17 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class levelFinishedCheck : MonoBehaviour
 {
+    public gameOverScreen gameOver;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.GetComponent<playerConrol>()) ;
+        if (collision.gameObject.GetComponent<playerConrol>()) 
         {
             //level complete
             Debug.Log("level finished");
             
             //int currentSceneNumber = SceneManager.GetActiveScene().buildIndex;
            // SceneManager.LoadScene(currentSceneNumber + 1);
+           
+            gameOver.playerDead();
             levelManager.Instance.markCurrentLevelComplete();
-
         }
     }
 }
